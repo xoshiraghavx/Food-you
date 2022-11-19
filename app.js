06353bc48bdcfa1837c27a03e173218d9780d0f1
OSHI RAGHAV <95966191+xoshiraghavx@users.noreply.github.com>
2022-11-19 15:04:58 +0530

    added modal

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ const links = document.querySelector(".links");
 navToggle.addEventListener('click', () =>{
     links.classList.toggle("show-links")
 })
+
+
+// modal js
+
+const modalBtn = document.querySelector(".modal-btn");
+const modal      = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener('click',() =>{
+    modal.classList.add('open-modal');
+})
+closeBtn.addEventListener('click',() =>{
+    modal.classList.remove('open-modal');
+})
