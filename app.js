5f8290920bc265c305e49579616f00db99510397
OSHI RAGHAV <95966191+xoshiraghavx@users.noreply.github.com>
2022-11-20 13:17:03 +0530

    added new section

--- app.js
+++ app.js
@@ -24,3 +24,14 @@ modalBtn.addEventListener('click',() =>{
 closeBtn.addEventListener('click',() =>{
     modal.classList.remove('open-modal');
 })
+
+//questions css
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const question =( e.currentTarget.parentElement.parentElement);
+        question.classList.toggle("show-text");
+    });
+});
